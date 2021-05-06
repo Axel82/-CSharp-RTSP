f816b3ef9b6da2b07935bc579bf2b39180944680
Axel82 <78731601+Axel82@users.noreply.github.com>
2021-05-06 13:21:16 +0200

    Initial commit

---
+++ VlcTest/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using AxAXVLC;
+
+namespace VlcTest
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        AxVLCPlugin2 vLCPlugin2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            vLCPlugin2 = new AxVLCPlugin2();
+            WFH1.Child = vLCPlugin2;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            vLCPlugin2.playlist.add("rtsp ://YOUR.RTSP.IP.000:PORT/h264Preview_01_sub", "LOGGIN", "PASSWORD");
+            vLCPlugin2.playlist.play();
+
+
+
+
+            //System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+            //ofd.ShowDialog();
+
+            //if(ofd.FileName != string.Empty)
+            //{
+            //    var myUri = new Uri(ofd.FileName);
+            //    var convertedUri = myUri.AbsoluteUri;
+
+            //    vLCPlugin2.playlist.add(convertedUri);
+            //    vLCPlugin2.playlist.play();
+            //}
+        }
+    }
+}
---
+++ VlcTest/Vlc.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace VlcTest
+{
+    partial class Vlc
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Vlc));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(0, 0);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(480, 320);
+            this.axVLCPlugin21.TabIndex = 0;
+            // 
+            // Vlc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "Vlc";
+            this.Size = new System.Drawing.Size(480, 320);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+    }
+}
